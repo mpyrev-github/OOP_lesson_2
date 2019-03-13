d3aeb463c0b73c7303a96ee13f12cbe117a0d505
Mikhail Pyrev <mihail.pyrev@gmail.com>
2019-03-14 02:12:39 +0500

    Add Part_1

---
+++ Part_1/ID/ID/ID.cpp
@@ -0,0 +1,13 @@
+﻿// Dll.cpp : Определяет экспортированные функции для приложения DLL.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "ID.h"
+
+using namespace std;
+
+void __stdcall NameAndGroup() {
+	cout << "Mikhail Pyrev" << endl << "RI-541216" << endl;
+	cin.get();
+}
---
+++ Part_1/ID/ID/ID.h
@@ -0,0 +1,3 @@
+#pragma once
+#define DLL_EXPORT extern "C" __declspec(dllexport)
+		DLL_EXPORT void __stdcall NameAndGroup();
